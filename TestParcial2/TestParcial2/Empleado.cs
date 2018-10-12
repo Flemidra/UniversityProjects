64f80b6a451e2854427953fca3070075c9fdd6c4
Flemidra <Flemidra@users.noreply.github.com>
2018-10-12 03:48:17 -0500

    Add files via upload

---
+++ TestParcial2/TestParcial2/Empleado.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ArrayEmpleado
+{
+    internal class Empleado
+    {
+        private string _nombreEmpleado;
+        private int _numeroEmpleado;
+        public Empleado()
+        {
+            _numeroEmpleado = 0;
+            _nombreEmpleado = "";
+        }
+        public Empleado(string nombreEmpleado, int numeroEmpleado)
+        {
+            _nombreEmpleado = nombreEmpleado;
+            _numeroEmpleado = numeroEmpleado;
+        }
+
+        public void LeerDato()
+        {
+            try
+            {
+                Console.WriteLine("Capture el numero de empleado.");
+                _numeroEmpleado = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese el nombre del empleado.");
+                _nombreEmpleado = Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Lo sentimos, ha ocurrido un error" + e.Message);
+            }
+        }
+
+        public void VerDatos()
+        {
+            Console.WriteLine("El numero de empleado es: " +_nombreEmpleado + " y el numero de empleado es: " + _numeroEmpleado);
+        }
+
+        
+    }
+}
---
+++ TestParcial2/TestParcial2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayEmpleado
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var lista = new List<Empleado>();
+            for (var i = 0; i < 3; i++)
+            {
+                var obj = new Empleado();
+                obj.LeerDato();
+                lista.Add(obj);
+            }
+
+            foreach (var value in lista)
+            {
+                value.VerDatos();
+            }
+            Console.ReadKey();
+        }
+    }
+}
+/*Crear una clase llamada empleado que contenga como miembro dato el nombre y numero de empleado y como metodos:
+ Leer datos() y ver datos(). 
+ Dichos metodos deben tomar los datos del teclado y los visualizara en pantalla respectivamente.
+ Considerar que debe utilizar, un array de tipo empleado y luego debera llenarlo con datos correspondientes a 50
+ empleados.
+ Por ultimo debera visualizar los datos de todos los empleados*/
